88823019d16190c0eea8e9dfc862ca4b508d902c
Mahesh Raskar <raskar.gwc@gmail.com>
2025-07-30 22:08:26 +0530

    Your commit message describing the changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000', // Django default port
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+    port: 5173, // or any port you want for local dev
+  },
 })
+
+// server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000', // Django default port
+//         changeOrigin: true,
+//         secure: false,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     }
+//   }
