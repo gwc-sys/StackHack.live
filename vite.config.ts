c39cb7adb3a78e68d927716f1865732af5b16462
Mahesh Raskar <raskar.gwc@gmail.com>
2025-08-04 22:35:01 +0530

    add post request API

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,40 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//        target: 'http://0.0.0.0:8000/', // Django default port
+//         changeOrigin: true,
+//         secure: false,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     },
+//     port: 5173, // Vite default port
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-       target: 'http://0.0.0.0:8000/', // Django default port
+        target: 'https://engiportal.onrender.com', // Production Django
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     },
-    port: 5173, // Vite default port
+    port: 5173,
   },
+  define: {
+    'import.meta.env.API_BASE_URL': JSON.stringify('https://engiportal.onrender.com')
+  }
 })
