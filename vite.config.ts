700aa6278f957f7f8cf1329291a70275b8df96de
Mahesh Raskar <raskar.gwc@gmail.com>
2025-08-15 12:29:44 +0530

    Updating Vite src

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,39 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//        target: 'http://0.0.0.0:8000/', // Django default port
-//         changeOrigin: true,
-//         secure: false,
-//         rewrite: (path) => path.replace(/^\/api/, '')
-//       }
-//     },
-//     port: 5173, // Vite default port
-//   },
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://engiportal.onrender.com', // Production Django
+       target: 'http://0.0.0.0:8000/', // Django default port
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     },
-    port: 5173,
+    port: 5173, // Vite default port
   },
-  define: {
-    'import.meta.env.API_BASE_URL': JSON.stringify('https://engiportal.onrender.com')
-  }
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://engiportal.onrender.com', // Production Django
+//         changeOrigin: true,
+//         secure: false,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     },
+//     port: 5173,
+//   },
+//   define: {
+//     'import.meta.env.API_BASE_URL': JSON.stringify('https://engiportal.onrender.com')
+//   }
+// })
