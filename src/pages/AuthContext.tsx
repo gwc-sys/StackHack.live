0a07532261a6eaf1a9068d0d43214a7682d7c7a6
Mahesh Raskar <raskar.gwc@gmail.com>
2025-09-01 19:41:48 +0530

    Ad

--- src/pages/AuthContext.tsx
+++ src/pages/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, useContext, ReactNode } from "react";
+import React, { createContext, useState, useEffect, useContext,  } from "react"; // ReactNode
 
 interface User {
   id: string;
