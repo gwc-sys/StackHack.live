c812f3c70203b35a2f3c782990e8c270bf3cf54f
Mahesh Raskar <raskar.gwc@gmail.com>
2025-09-15 19:48:47 +0530

    Updating

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",  // ✅ ensures assets load from root
+  base: "/",  
   server: {
     proxy: {
       '/api': {
